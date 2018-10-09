b91e58c64993167c897a6d5bc0904ae51e58c3bd
Pete Dring <pddring@googlemail.com>
2018-10-09 16:27:01 +0100

    Example Emgu OpenCV for FPV

--- TunerTest/Form1.Designer.vb
+++ TunerTest/Form1.Designer.vb
@@ -22,10 +22,117 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.txtCamNumber = New System.Windows.Forms.TextBox()
+        Me.groupCam = New System.Windows.Forms.GroupBox()
+        Me.btnDisconnect = New System.Windows.Forms.Button()
+        Me.groupPreview = New System.Windows.Forms.GroupBox()
+        Me.pbPreview = New System.Windows.Forms.PictureBox()
+        Me.lblFPS = New System.Windows.Forms.Label()
+        Me.timerFPS = New System.Windows.Forms.Timer(Me.components)
+        Me.groupCam.SuspendLayout()
+        Me.groupPreview.SuspendLayout()
+        CType(Me.pbPreview, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(6, 56)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(70, 29)
+        Me.btnConnect.TabIndex = 0
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'txtCamNumber
+        '
+        Me.txtCamNumber.Location = New System.Drawing.Point(6, 19)
+        Me.txtCamNumber.Name = "txtCamNumber"
+        Me.txtCamNumber.Size = New System.Drawing.Size(146, 20)
+        Me.txtCamNumber.TabIndex = 1
+        Me.txtCamNumber.Text = "0"
+        '
+        'groupCam
+        '
+        Me.groupCam.Controls.Add(Me.btnDisconnect)
+        Me.groupCam.Controls.Add(Me.btnConnect)
+        Me.groupCam.Controls.Add(Me.txtCamNumber)
+        Me.groupCam.Location = New System.Drawing.Point(12, 12)
+        Me.groupCam.Name = "groupCam"
+        Me.groupCam.Size = New System.Drawing.Size(166, 100)
+        Me.groupCam.TabIndex = 2
+        Me.groupCam.TabStop = False
+        Me.groupCam.Text = "Camera number:"
+        '
+        'btnDisconnect
+        '
+        Me.btnDisconnect.Location = New System.Drawing.Point(82, 56)
+        Me.btnDisconnect.Name = "btnDisconnect"
+        Me.btnDisconnect.Size = New System.Drawing.Size(70, 29)
+        Me.btnDisconnect.TabIndex = 2
+        Me.btnDisconnect.Text = "Disconnect"
+        Me.btnDisconnect.UseVisualStyleBackColor = True
+        '
+        'groupPreview
+        '
+        Me.groupPreview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.groupPreview.Controls.Add(Me.pbPreview)
+        Me.groupPreview.Location = New System.Drawing.Point(12, 118)
+        Me.groupPreview.Name = "groupPreview"
+        Me.groupPreview.Size = New System.Drawing.Size(783, 327)
+        Me.groupPreview.TabIndex = 3
+        Me.groupPreview.TabStop = False
+        Me.groupPreview.Text = "Preview"
+        '
+        'pbPreview
+        '
+        Me.pbPreview.Location = New System.Drawing.Point(6, 19)
+        Me.pbPreview.Name = "pbPreview"
+        Me.pbPreview.Size = New System.Drawing.Size(529, 308)
+        Me.pbPreview.TabIndex = 0
+        Me.pbPreview.TabStop = False
+        '
+        'lblFPS
+        '
+        Me.lblFPS.AutoSize = True
+        Me.lblFPS.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFPS.Location = New System.Drawing.Point(195, 22)
+        Me.lblFPS.Name = "lblFPS"
+        Me.lblFPS.Size = New System.Drawing.Size(102, 33)
+        Me.lblFPS.TabIndex = 4
+        Me.lblFPS.Text = "Label1"
+        '
+        'timerFPS
+        '
+        Me.timerFPS.Enabled = True
+        Me.timerFPS.Interval = 500
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblFPS)
+        Me.Controls.Add(Me.groupPreview)
+        Me.Controls.Add(Me.groupCam)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.groupCam.ResumeLayout(False)
+        Me.groupCam.PerformLayout()
+        Me.groupPreview.ResumeLayout(False)
+        CType(Me.pbPreview, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents txtCamNumber As TextBox
+    Friend WithEvents groupCam As GroupBox
+    Friend WithEvents btnDisconnect As Button
+    Friend WithEvents groupPreview As GroupBox
+    Friend WithEvents pbPreview As PictureBox
+    Friend WithEvents lblFPS As Label
+    Friend WithEvents timerFPS As Timer
 End Class
--- TunerTest/Form1.vb
+++ TunerTest/Form1.vb
@@ -1,3 +1,48 @@
-﻿Public Class Form1
+﻿Imports Emgu.CV
+Public Class Form1
+    Dim camera As Emgu.CV.VideoCapture
+    Dim img As Emgu.CV.Mat
+    Dim frameCount As Integer = 0
 
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        Dim camNumber As Integer = txtCamNumber.Text
+        camera = New Emgu.CV.VideoCapture(camNumber)
+        img = New Emgu.CV.Mat()
+
+        camera.Start()
+        AddHandler camera.ImageGrabbed, AddressOf FrameReceived
+
+    End Sub
+
+    Private Sub FrameReceived(sender As Object, e As EventArgs)
+        camera.Retrieve(img)
+        pbPreview.Invalidate()
+        frameCount += 1
+    End Sub
+
+    Sub btnDisconnect_Click(sender As Object, e As EventArgs) Handles btnDisconnect.Click
+        camera.Stop()
+        camera.Dispose()
+    End Sub
+
+    Private Sub pbPreview_Paint(sender As Object, e As PaintEventArgs) Handles pbPreview.Paint
+        If Not IsNothing(img) Then
+            If img.Width <> pbPreview.Width Then
+                pbPreview.Width = img.Width
+                groupPreview.Width = img.Width
+            End If
+            If img.Height <> pbPreview.Height Then
+                pbPreview.Height = img.Height
+                groupPreview.Height = img.Height
+            End If
+
+            e.Graphics.DrawImage(img.Bitmap, New PointF(0, 0))
+
+        End If
+    End Sub
+
+    Private Sub timerFPS_Tick(sender As Object, e As EventArgs) Handles timerFPS.Tick
+        lblFPS.Text = (frameCount * 2) & "FPS"
+        frameCount = 0
+    End Sub
 End Class
